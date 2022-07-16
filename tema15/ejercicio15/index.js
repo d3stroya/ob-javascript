0d3af82f71266b4fc3f7c88b311b1a475a0b19dd
d3stroya <destroya@users.noreplay.github.com>
2022-07-16 20:56:32 +0200

    getTime

--- tema15/ejercicio15/index.js
+++ tema15/ejercicio15/index.js
@@ -13,8 +13,8 @@ sessionStorage.setItem("nombreCompleto", JSON.stringify(objeto))
 localStorage.setItem("nombreCompleto", JSON.stringify(objeto))
 
 // Crea una cookie que caduque en 2 minutos con los datos del objeto anterior
-date = new Date();
+now = new Date();
 document.cookie="cookie=nombreCompleto;expires" 
-+ date.setTime(date.getTime() + 20 * 60 * 1000).toUTCString()
++ new Date(now.getTime() + 20 * 60 * 1000).toUTCString()
 
 
